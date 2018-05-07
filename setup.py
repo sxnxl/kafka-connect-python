8c02fa17102280fd93d0a231c82643f455483834
Senol Korkmaz <senol.korkmaz@gmail.com>
2018-05-07 20:14:45 +0300

    initial implementation

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='kafka-connect-python',
+      version='0.1',
+      description='Kafka Connect Module',
+      url='http://github.com/sxnxl/kafka-connect-python',
+      author='Senol Korkmaz',
+      author_email='senol.korkmaz@gmail.com',
+      license='MIT',
+      packages=['kafka_connect'],
+      classifiers=[  # Optional
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+
+          'Programming Language :: Python :: 3',
+      ],
+      )
